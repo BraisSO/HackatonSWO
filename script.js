563a5e9a0b40a6d1c4de92df2777a2af0bbebd5d
Brais <103991765+BraisSO@users.noreply.github.com>
2023-04-18 13:57:35 +0200

    Barra lateral + transcripción

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// DECODER
-$(document).ready(function() {
-    $(".desplazar").click(function() {
-      $(".decode-container").toggleClass("show hide");
+// Movimiento Decoder
+$(document).ready(function () {
+    $(".desplazar").click(function () {
+        $(".decode-container").toggleClass("show hide");
     });
-  });
+});
 
-  // LINK PARA INFO
+
+//Decoder functionality
+$(document).ready(function () {
+    $('#decodificar').click(function (e) {
+        e.preventDefault();
+        var binaryInput = $('#codigo').val().replace(/\s+/g, '');
+        var textOutput = '';
+
+        for (var i = 0; i < binaryInput.length; i += 8) {
+            var byte = binaryInput.substr(i, 8);
+            textOutput += String.fromCharCode(parseInt(byte, 2));
+        }
+
+        $('#result').text(textOutput);
+    });
+});
